ea2763c7f22e853146f7f393a1aeb35195210c3a
Nicolas Setién <146782367+NicolasSetien@users.noreply.github.com>
2023-12-23 09:37:16 -0400

    Value objects de Track

--- lib/domain/multimedia_related/track.dart
+++ lib/domain/multimedia_related/track.dart
@@ -1,19 +1,76 @@
-import 'Artist.dart';
+
+// class Track {
+//   Track(
+//     this.id,
+//     this.name,
+//     this.compositor,
+//     this.imageURL,
+//     this.duration, {
+//     this.artist,
+//   });
+//
+//   final String id;
+//   final String name;
+//   final String compositor;
+//   final String imageURL;
+//   final double duration;
+//   Artist? artist;
+// }
 
 class Track {
   Track(
-    this.id,
-    this.name,
-    this.compositor,
-    this.imageURL,
-    this.duration, {
-    this.artist,
-  });
+      this.id,
+      this.name,
+      this.url,
+      this.imageURL,
+      this.date,
+      this.previewURL,
+      this.genres,
+      );
 
-  final String id;
+  final String id; //cambiar a Idsong
+  final TrackName name;
+  final TrackReference url;
+  final TrackImage imageURL;
+  final TrackCreationDate date;
+  final TrackPreview previewURL;
+  final List<TrackGenre> genres;
+}
+
+class TrackName {
+  TrackName(this.name);
+
   final String name;
-  final String compositor;
+}
+
+class TrackImage {
+  TrackImage(this.imageURL);
+
   final String imageURL;
-  final double duration;
-  Artist? artist;
+}
+
+class TrackReference {
+  TrackReference(this.url);
+
+  final String url;
+}
+
+class TrackCreationDate {
+  TrackCreationDate(this.date){
+    assert(date.isBefore(DateTime.now()));
+  }
+
+  final DateTime date;
+}
+
+class TrackPreview {
+  TrackPreview(this.previewURL);
+
+  final String previewURL;
+}
+
+class TrackGenre {
+  TrackGenre(this.genre);
+
+  final String genre;
 }
