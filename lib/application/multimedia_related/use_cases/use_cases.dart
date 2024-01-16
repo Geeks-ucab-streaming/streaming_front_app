cbc8154c48632312c8d10a9ea6060b0013fa8a9a
Alejandro Novellino <alnovellino07@gmail.com>
2024-01-15 20:14:32 -0400

    add page

--- lib/application/multimedia_related/use_cases/use_cases.dart
+++ lib/application/multimedia_related/use_cases/use_cases.dart
@@ -2,3 +2,4 @@ export './home_info/get_home_info.dart';
 export './artist_info/get_artist_info.dart';
 export './playlist_info/get_playlist_info.dart';
 export './album_info/get_album_info.dart';
+export './search_info/get_search_info.dart';
