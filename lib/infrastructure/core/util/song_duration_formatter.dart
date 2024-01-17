8de087496220c7324c8a9784be5e983d67408469
Nicolas Setién <146782367+NicolasSetien@users.noreply.github.com>
2024-01-17 16:01:08 -0400

    Duration formatter bug fixed

--- lib/infrastructure/core/util/song_duration_formatter.dart
+++ lib/infrastructure/core/util/song_duration_formatter.dart
@@ -5,9 +5,9 @@ class SongDurationFormatter {
     try {
       var reversed = songDuration.split('').reversed.join();
       if (reversed[4] == '0') {
-        formattedDuration = songDuration.substring(0, 4).split('').reversed.join();
+        formattedDuration = reversed.substring(0, 4).split('').reversed.join();
       } else {
-        formattedDuration = songDuration.substring(0, 5).split('').reversed.join();
+        formattedDuration = reversed.substring(0, 5).split('').reversed.join();
       }
     } catch (e) {
       formattedDuration = songDuration;
