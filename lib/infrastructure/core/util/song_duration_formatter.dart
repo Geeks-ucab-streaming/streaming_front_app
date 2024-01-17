cd7e65a543bc903d708259d372295ed78145b7f5
Alejandro Novellino <alnovellino07@gmail.com>
2024-01-17 18:17:00 -0400

    remove print from song formatter

--- lib/infrastructure/core/util/song_duration_formatter.dart
+++ lib/infrastructure/core/util/song_duration_formatter.dart
@@ -4,7 +4,7 @@ class SongDurationFormatter {
     // try to format duration
     try {
       var duration = songDuration.split(':');
-      print(duration);
+
       if (duration[0] == '00') {
         formattedDuration = songDuration.substring(4, 8);
       } else if (duration[0].startsWith('0')) {
