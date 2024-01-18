6355adc1e4cf16512291876115098fcd4eb7fba8
Alejandro Novellino <alnovellino07@gmail.com>
2024-01-18 00:05:05 -0400

    socket updated

--- lib/infrastructure/core/audioPlayer/SocketManager.dart
+++ lib/infrastructure/core/audioPlayer/SocketManager.dart
@@ -55,8 +55,7 @@ class SocketManager {
     final localStorage = getIt<LocalStorageInstance>().getLocalStorage();
     await localStorage.ready;
     String? token = await localStorage.getItem('token');
-    String? guestToken = await localStorage.getItem('lastGuestToken');
-    return (token != null) ? token : guestToken as String;
+    return token as String;
   }
 
   Future<void> requestSongToServer(String currentSongid) async {
